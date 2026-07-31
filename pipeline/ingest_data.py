36dc64101e1ee0c7f9518335b92f928df3532bbe
dtrejo0611 <dtrejot021106@gmail.com>
2026-07-30 18:31:09 -0600

    docker + postgresql

--- pipeline/ingest_data.py
+++ pipeline/ingest_data.py
@@ -4,6 +4,7 @@
 import pandas as pd
 from sqlalchemy import create_engine
 from tqdm.auto import tqdm
+import click
 
 dtype = { 
     "VendorID": "Int64",
@@ -30,19 +31,17 @@ parse_dates = [
 ]
 
 
-def run():
-    pg_user = 'root'
-    pg_password = 'root'
-    pg_host = 'localhost'
-    pg_port = 5432
-    pg_db = 'ny_taxi'
-
-    year = 2021
-    month = 1
-
-    table_name = 'yellow_taxi_data'
-
-    chunk_size = 100000
+@click.command()
+@click.option('--pg-user', default='root', show_default=True, help='Postgres user')
+@click.option('--pg-password', default='root', show_default=True, help='Postgres password')
+@click.option('--pg-host', default='localhost', show_default=True, help='Postgres host')
+@click.option('--pg-port', default=5432, show_default=True, type=int, help='Postgres port')
+@click.option('--pg-db', default='ny_taxi', show_default=True, help='Postgres database')
+@click.option('--year', default=2021, show_default=True, type=int, help='Year of the dataset')
+@click.option('--month', default=1, show_default=True, type=int, help='Month of the dataset')
+@click.option('--table-name', default='yellow_taxi_data', show_default=True, help='Target table name')
+@click.option('--chunk-size', default=100000, show_default=True, type=int, help='Chunk size for CSV iterator')
+def run(pg_user, pg_password, pg_host, pg_port, pg_db, year, month, table_name, chunk_size):
 
     prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/'
     url = f'{prefix}yellow_tripdata_{year}-{month:02d}.csv.gz'
